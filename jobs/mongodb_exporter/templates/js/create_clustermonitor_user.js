590f164e6d9d48ccb030124cee6b051127a8d362
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2018-09-13 12:27:10 +0200

    Wrong password in js

--- jobs/mongodb_exporter/templates/js/create_clustermonitor_user.js
+++ jobs/mongodb_exporter/templates/js/create_clustermonitor_user.js
@@ -2,12 +2,12 @@ use admin;
 if (db.system.users.find({ user: "<%= p('mongodb.clustermonitor.username') %>" }).count() == 0) {
     db.createUser({
         user: "<%= p('mongodb.clustermonitor.username') %>",
-        pwd: "<%= p('mongodb.clustermonitor.username') %>",
+        pwd: "<%= p('mongodb.clustermonitor.password') %>",
         roles: [
             { role: "clusterMonitor", db: "admin" },
         	{ role: "read", db: "local" }
         ]
     });
 } else {
-    db.changeUserPassword("<%= p('mongodb.clustermonitor.username') %>", "<%= p('mongodb.clustermonitor.username') %>");
+    db.changeUserPassword("<%= p('mongodb.clustermonitor.username') %>", "<%= p('mongodb.clustermonitor.password') %>");
 }
