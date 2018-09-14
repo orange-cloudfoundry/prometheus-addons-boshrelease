4684831c865a32e5d5548170c5d9e536c5769c0f
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2018-09-14 22:38:52 +0200

    ClusterMonitor not created

--- jobs/mongodb_exporter/templates/bin/create_clustermonitor_user.sh
+++ jobs/mongodb_exporter/templates/bin/create_clustermonitor_user.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -eu
 
+<%-
+  require "shellwords"
+
+  def esc(x)
+    Shellwords.shellescape(x)
+  end
+
+  _instances = ''
+  link('exporter').instances.each do |instance|
+    _instances = "#{_instances}#{instance.address} "
+  end
+  # remove last space
+  instances = _instances[0..-2]
+  current_ip = spec.ip
+%>
+
 JOB_DIR=/var/vcap/jobs/mongodb_exporter
 
 # Add all packages' /bin & /sbin into $PATH
@@ -21,9 +37,9 @@ PORT=$(echo <%= uri %>|sed -e "s/^.*@[^:]*:\([0-9]*\).*/\1/")
 <%- end %>
 # waiting for port availability
 
-timeout=120
+Timeout=120
 reached=0
-while [ ${timeout} -gt 0 ] && [ ${reached} -ne 1 ]
+while [ ${Timeout} -gt 0 ] && [ ${reached} -ne 1 ]
 do
 	if nc -z 127.0.0.1 "${PORT}"
 	then
@@ -32,10 +48,10 @@ do
 	if [ ${reached} -eq 0 ]
 	then
 		sleep 1
-		timeout=$(($timeout-1))
+		Timeout=$(($Timeout-1))
 	fi
 done
-[ ${timeout} -le 0 ] \
+[ ${Timeout} -le 0 ] \
 			&& echo "Cannot connect to mongodb on localhost:${PORT}. Timeout reached" \
 			&& exit 1
 
@@ -50,8 +66,8 @@ CONNECT_STRING="mongodb://<%= p('mongodb.root.username') %>:<%= p('mongodb.root.
 # wait for root user availability
 
 root_initialized=0
-timeout=120
-while [ ${timeout} -gt 0 -a ${root_initialized} -eq 0 ]
+Timeout=120
+while [ ${Timeout} -gt 0 -a ${root_initialized} -eq 0 ]
 do
     root_initialized=1
     
@@ -59,18 +75,36 @@ do
     		--eval 'Mongo()'||echo 1)|grep 'connection to 127.0.0.1:'${PORT}|wc -l)" -eq 0 ] 
 	then
 		sleep 1
-		timeout=$(($timeout-1))
+		Timeout=$(($Timeout-1))
 		root_initialized=0
 	fi
 done
 
-[ ${timeout} -le 0 ] \
+[ ${Timeout} -le 0 ] \
 			&& echo "Cannot connect to mongodb on localhost:${PORT} with <%= p('mongodb.root.username') %> user. Timeout reached" \
 			&& exit 1
 
 # Perform user creation on master node only
+Master_is_identified=0
+Timeout=120
+while [ ${Master_is_identified} -eq 0 -a ${Timeout} -gt 0 ]
+do
+	for i in <%= instances %>
+	do
+		CONNECT_STRING_CHECK=$(echo ${CONNECT_STRING}|sed -e "s/127.0.0.1/$i/")
+		if [ "$(${MONGO_CMD} ${CONNECT_STRING_CHECK} --eval 'rs.isMaster().ismaster')" == "true" ]
+		then
+			Master=$i
+			Master_is_identified=1
+		fi
+	done
+	Timeout=$((Timeout-1))
+done
+[ ${Timeout} -le 0 ] \
+			&& echo "Cannot find a valid master node within <%= instances %>. Timeout reached" \
+			&& exit 1
 
-if [ "$(${MONGO_CMD} ${CONNECT_STRING} --eval 'rs.isMaster().ismaster')" == "true" ]
+if [ "${Master}" == "<%= esc(current_ip) %>" ]
 then
-     exec ${MONGO_CMD} ${CONNECT_STRING} < $JOB_DIR/js/create_clustermonitor_user.js
+    ${MONGO_CMD} ${CONNECT_STRING} $JOB_DIR/js/create_clustermonitor_user.js
 fi
